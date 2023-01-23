ac93f0760b1e6bf3622ed718d2ebc3aa1b84103f
2horse9sun <jsjhfx@163.com>
2023-01-23 16:01:53 +0200

    first commit

--- data/data_import.py
+++ data/data_import.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from bson import json_util
+from bson import ObjectId
 import json
 
 
@@ -15,28 +15,28 @@ print("Start to import...")
 with open("rule_template.json", encoding='utf-8') as file:
     rule_template_json = json.load(file)
     for rule_template in rule_template_json:
-        rule_template['_id'] = rule_template['_id']['$oid']
+        rule_template['_id'] = ObjectId(rule_template['_id']['$oid'])
     print(rule_template_json)
     rule_template_collection.insert_many(rule_template_json)
     
 with open("rule.json", encoding='utf-8') as file:
     rule_json = json.load(file)
     for rule in rule_json:
-        rule['_id'] = rule['_id']['$oid']
+        rule['_id'] = ObjectId(rule['_id']['$oid'])
     print(rule_json)
     rule_collection.insert_many(rule_json)
     
 with open("rule_group.json", encoding='utf-8') as file:
     rule_group_json = json.load(file)
     for rule_group in rule_group_json:
-        rule_group['_id'] = rule_group['_id']['$oid']
+        rule_group['_id'] = ObjectId(rule_group['_id']['$oid'])
     print(rule_group_json)
     rule_group_collection.insert_many(rule_group_json)
     
 with open("location_rule_group.json", encoding='utf-8') as file:
     location_rule_group_json = json.load(file)
     for location_rule_group in location_rule_group_json:
-        location_rule_group['_id'] = location_rule_group['_id']['$oid']
+        location_rule_group['_id'] = ObjectId(location_rule_group['_id']['$oid'])
     print(location_rule_group_json)
     location_rule_group_collection.insert_many(location_rule_group_json)
 
